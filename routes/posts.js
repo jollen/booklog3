b699c19c6d762eac73124122da1bd8558c5a867a
Afoo Chang <foofoo.chang@gmail.com>
2015-07-25 14:16:23 +0800

    require winston, #7

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var events = require('events');
+var winston = require('winston');
 
 function ensureAuthenticate(req, res, next) {
   if (req.isAuthenticated()) {
